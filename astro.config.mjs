fa64b1cbba03e2768c1f3459cc95ac16489bfdaa
J0na555 <ethiopediaa@gmail.com>
2025-12-27 23:06:35 +0300

    Add TypeScript, Astro, and Tailwind configuration files

---
+++ astro.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+import vercel from "@astrojs/vercel";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  output: "server",
+  adapter: vercel(),
+  vite: {
+    resolve: {
+      alias: {
+        "@": "/src",
+        "@components": "/src/components",
+      },
+    },
+  },
+  build: {
+    inlineStylesheets: "auto",
+  },
+  server: {
+    host: true,
+    port: 4321,
+  },
+});
